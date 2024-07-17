a23aabdd9507358e4f5930f3dadc78feae79cac0
Andrei Borca <andreiborca@gmail.com>
2024-07-17 14:36:17 +0300

    feat: Add LogLevel enum and it's unit test

--- tests/unit/Enums/LogLevelTest.php
+++ tests/unit/Enums/LogLevelTest.php
@@ -3,6 +3,7 @@
 namespace Tests\unit\Enums;
 
 use App\Enums\LogLevel;
+use App\Errors\InvalidLogLevelError;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -20,6 +21,29 @@ final class LogLevelTest extends TestCase
         $this->assertEquals("CRITICAL",LogLevel::CRITICAL);
     }
 
+    public function testConstantsValueToInt(): void
+    {
+        $this->assertEquals(0,LogLevel::toInt(LogLevel::DEBUG));
+        $this->assertEquals(1,LogLevel::toInt(LogLevel::INFO));
+        $this->assertEquals(2,LogLevel::toInt(LogLevel::WARNING));
+        $this->assertEquals(3,LogLevel::toInt(LogLevel::ERROR));
+        $this->assertEquals(4,LogLevel::toInt(LogLevel::CRITICAL));
+    }
 
+    public function testConstantsValueToIntError(): void
+    {
+        $this->expectException(InvalidLogLevelError::class);
+        LogLevel::toInt("INVALID_LOG_Level");
+    }
 
+    public function testSupportedLogLevels(): void {
+        $expectedSupportedLogLevels = [
+            LogLevel::DEBUG,
+            LogLevel::INFO,
+            LogLevel::WARNING,
+            LogLevel::ERROR,
+            LogLevel::CRITICAL,
+        ];
+        $this->assertEquals($expectedSupportedLogLevels, LogLevel::getSupportedLogLevels());
+    }
 }
