bfd8887b9a140feaecdf6db8fdb682891000cf15
Andrei Borca <andreiborca@gmail.com>
2024-07-18 20:30:32 +0300

    feat: Add configuration file and logic to handle the initialization of logger service based on configuration

---
+++ app/services/LoggingServiceProvider.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\services;
+
+use App\Interfaces\LoggerDriverFactoryInterface;
+use App\Interfaces\LoggerInterface;
+use Exception;
+use Symfony\Component\Yaml\Yaml;
+
+class LoggingServiceProvider
+{
+    private array $config;
+    private LoggerDriverFactoryInterface $driverFactory;
+    private LoggerInterface $logger;
+
+    /**
+     * LoggingServiceProvider constructor.
+     *
+     * @param LoggerDriverFactoryInterface $driverFactory
+     * @param string $configPath
+     *
+     * @throws Exception
+     */
+    public function __construct(
+        LoggerDriverFactoryInterface $driverFactory,
+        string $configPath = "config/telemetry_logging.yml",
+    ) {
+        if (empty(trim($configPath))) {
+            throw new Exception("Configuration file path for telemetry logging is empty");
+        }
+
+        if (!file_exists($configPath)) {
+            throw new Exception(sprintf(
+                "Could not find the configuration file based on provided path `%s`",
+                $configPath
+            ));
+        }
+
+        $this->config = Yaml::parseFile($configPath);
+        $this->driverFactory = $driverFactory;
+
+        $this->initializeLogger();
+    }
+
+    private function initializeLogger() {
+        $defaultDriver = $this->config['default'];
+        $driversConfig = $this->config['drivers'];
+
+        // TODO: add validation of configuration that configuration for the
+        // mentioned driver under default key exists under drivers key
+
+        $driver = $this->driverFactory->initDriver($defaultDriver, $driversConfig[$defaultDriver]);
+
+        $this->logger = new Logger($driver);
+    }
+
+    public function getLogger(): LoggerInterface {
+        return $this->logger;
+    }
+}
