c448365c0360666ad40cb1136dcb9215d8c66b12
Andrei Borca <andreiborca@gmail.com>
2024-07-26 14:14:24 +0300

    feat: Add semnatic versioning package configuration

---
+++ release.config.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+    branches: [
+        {'name': 'main', 'channel': 'latest', 'prerelease': false},
+        {'name': 'feature/semantic-versioning', 'channel': 'latest', 'prerelease': false},
+        {'name': 'master', 'channel': 'latest', 'prerelease': false},
+        {'name': 'beta', 'prerelease': true},
+    ],
+    debug: true,
+    plugins: [
+        ['@semantic-release/commit-analyzer', {
+            preset: 'angular',
+            parserOpts: {
+                headerPattern: /(\w*)(?:\((.*)\))?: (.*)$/,
+            }
+        }],
+        ["@semantic-release/release-notes-generator", {
+            preset: 'angular',
+            parserOpts: {
+                headerPattern: /(\w*)(?:\((.*)\))?: (.*)$/,
+            }
+        }],
+        [
+            "@semantic-release/changelog", {
+            "changelogFile": "CHANGELOG.md"
+        }
+        ],
+        // Enable the next line if the project is a GitHub / NPM package
+        // "@semantic-release/npm",
+        "@semantic-release/github",
+        [
+            "@semantic-release/git",
+            {
+                "assets": ["composer.json", "package.json", "package-lock.json", "CHANGELOG.md"],
+                "message": "chore(release): ${nextRelease.version} \n\n${nextRelease.notes}"
+            }
+        ]
+    ],
+};
